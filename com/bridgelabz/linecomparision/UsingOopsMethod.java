c97341a3aec3c874b36f80ea011f5728f06281af
rishita1234 <rishita123@gmail.com>
2022-12-15 04:00:56 +0530

    Added: Using Oops Method to compare lines are equal

---
+++ com/bridgelabz/linecomparision/UsingOopsMethod.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.linecomparision;
+
+public class UsingOopsMethod {
+public static  double line1,line2;
+    public static  void calSqrt() {
+        double x1 = 13, x2 =25 , y1 = 2, y2 = 6;
+         line1 = Math.sqrt((x2 - x1) + (y2 - y1));
+        System.out.println(line1);
+         line2 = Math.sqrt((x2 - x1) - (y2 - y1));
+        System.out.println(line2);
+    }
+
+    public static void checkEqual() {
+        if (line1 == line2) {
+            System.out.println("Two line are equal");
+        } else {
+            System.out.println("Two line are not equal");
+        }
+    }
+
+    public static void compareLines() {
+        if (line1 >line2) {
+            System.out.println("Line1 is the maximum");
+        } else {
+            System.out.println("Line2 is the maximum");
+        }
+    }
+
+    public static void main(String[] args) {
+        calSqrt();
+        checkEqual();
+        compareLines();
+    }
+}
